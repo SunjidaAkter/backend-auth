808e3a0de12771baa1762b39f7647ba38110221c
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-10-11 18:27:53 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+// const toursRoute = require("./routes/v1/tours.route")
+// const tourRoute = require("./routes/v1/tour.route")
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+//checking routes
+app.get("/", (req, res) => {
+    res.send("working properly!");
+})
+
+
+
+module.exports = app;
